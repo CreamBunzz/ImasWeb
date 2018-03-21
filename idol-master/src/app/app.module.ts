a096285ea9af904e76e2de8487169942454ca7d1
DESKTOP-89FPSE5\kazuy <creambunzz@yahoo.co.jp>
2018-03-21 13:49:22 +0900

    ヘッダー(作成)

--- idol-master/src/app/app.module.ts
+++ idol-master/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderModule } from './header/header.module';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
